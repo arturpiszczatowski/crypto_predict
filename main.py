555bb4b7e6b768121b2defed3bd98bb29a7f0aca
Zevs <piszczatowski.artur@gmail.copm>
2022-02-04 00:17:42 +0100

    training models

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-import pandas as pd
 import os
+from datetime import datetime
 
-# last 60 minutes of data
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import pandas as pd
+from sklearn import preprocessing
+from collections import deque
+import numpy as np
+import random
+import tensorflow as tf
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM, BatchNormalization
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
+
 SEQ_LEN = 60
-# predict 3 minutes into the future
 FUTURE_PERIOD_PREDICT = 3
-RATIO_TO_PREDICT = "LTC-USD"
+RATIO_TO_PREDICT = "BCH-USD"
+EPOCHS = 10
+BATCH_SIZE = 64
+NAME = f"{RATIO_TO_PREDICT}-{SEQ_LEN}-SEQ-{FUTURE_PERIOD_PREDICT}-PRED-{datetime.now().strftime('%d%m%Y-%H-%M-%S')}"
 
 
 def classify(current, future):
@@ -13,6 +27,57 @@ def classify(current, future):
         return 1
     else:
         return 0
+
+
+def preprocess_df(df):
+    df = df.drop('future', 1)
+
+    for col in df.columns:
+        if col != "target":
+            df[col] = df[col].pct_change()
+            df.dropna(inplace=True)
+            df[col] = preprocessing.scale(df[col].values)
+
+    df.dropna(inplace=True)
+
+    sequential_data = []
+    prev_days = deque(maxlen=SEQ_LEN)
+
+    for i in df.values:
+        prev_days.append([n for n in i[:-1]])
+        if len(prev_days) == SEQ_LEN:
+            sequential_data.append([np.array(prev_days), i[-1]])
+    random.shuffle(sequential_data)
+
+    buys = []
+    sells = []
+
+    for seq, target in sequential_data:
+        if target == 0:
+            sells.append([seq, target])
+        elif target == 1:
+            buys.append([seq, target])
+
+    random.shuffle(buys)
+    random.shuffle(sells)
+
+    lower = min(len(buys), len(sells))
+
+    buys = buys[:lower]
+    sells = sells[:lower]
+
+    sequential_data = buys + sells
+
+    random.shuffle(sequential_data)
+
+    x = []
+    y = []
+
+    for seq, target in sequential_data:
+        x.append(seq)
+        y.append(target)
+
+    return np.array(x).astype("float32"), np.array(y)
 
 
 main_df = pd.DataFrame()
@@ -32,7 +97,59 @@ for ratio in ratios:
     else:
         main_df = main_df.join(df)
 
+main_df.fillna(method="ffill", inplace=True)
+main_df.dropna(inplace=True)
+
 main_df['future'] = main_df[f"{RATIO_TO_PREDICT}_close"].shift(-FUTURE_PERIOD_PREDICT)
 main_df['target'] = list(map(classify, main_df[f"{RATIO_TO_PREDICT}_close"], main_df["future"]))
 print(main_df[[f"{RATIO_TO_PREDICT}_close", "future", "target"]].head(10))
 
+times = sorted(main_df.index.values)
+last_5pct = times[-int(0.05 * len(times))]
+
+validation_main_df = main_df[(main_df.index >= last_5pct)]
+main_df = main_df[(main_df.index < last_5pct)]
+
+train_x, train_y = preprocess_df(main_df)
+validation_x, validation_y = preprocess_df(validation_main_df)
+
+model = Sequential()
+model.add(LSTM(128, input_shape=(train_x.shape[1:]), return_sequences=True))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(LSTM(128, input_shape=(train_x.shape[1:]), return_sequences=True))
+model.add(Dropout(0.1))
+model.add(BatchNormalization())
+
+model.add(LSTM(128, input_shape=(train_x.shape[1:])))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Dense(32, activation="relu"))
+model.add(Dropout(0.2))
+
+model.add(Dense(2, activation="softmax"))
+
+opt = tf.keras.optimizers.Adam(learning_rate=0.001, decay=1e-6)
+
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer=opt,
+              metrics=['accuracy'])
+
+tensorboard = TensorBoard(log_dir=f'logs/{NAME}')
+
+filepath = "RNN_Final-{epoch:02d}-{val_accuracy:.3f}.hd5"
+
+checkpoint = ModelCheckpoint("models/{}_{}".format(NAME, filepath),
+                             monitor='val_accuracy',
+                             verbose=1,
+                             save_best_only=True,
+                             mode='max')
+
+history = model.fit(
+    train_x, train_y,
+    batch_size=BATCH_SIZE,
+    epochs=EPOCHS,
+    validation_data=(validation_x, validation_y),
+    callbacks=[tensorboard, checkpoint])
